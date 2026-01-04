740a0df170051bd90e40cbdafee0e993bae8e4e5
Aldo <jorti104@mtroyal.ca>
2026-01-04 03:22:39 -0700

    Fix React Router warnings and remove Tailwind CDN

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout.tsx';
+import Landing from './views/Landing.tsx';
+import MapView from './views/MapView.tsx';
+import ShareView from './views/ShareView.tsx';
+import Guidelines from './views/Guidelines.tsx';
+
+const App: React.FC = () => {
+  return (
+    <Router
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/map" element={<MapView />} />
+          <Route path="/share" element={<ShareView />} />
+          <Route path="/guidelines" element={<Guidelines />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
+};
+
+export default App;
